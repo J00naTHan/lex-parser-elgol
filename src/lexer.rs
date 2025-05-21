ade04bc04c5b017a5aee6911783c833c014c8de9
J00naTHan <jonathan.dutra.2004@gmail.com>
2025-05-21 17:23:10 -0300

    Versão 2.0

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,26 +1,55 @@
-use logos::Logos;
+// importa a crate logos (structs Lexer, Logos e Skip) no escopo do analisador léxico (lexer)
+use logos::{Lexer, Logos, Skip};
 
+
+/// atualiza o contador de linhas e colunas do código-fonte usando caracteres de quebra de linha
+// baseado no exemplo fornecido em: https://logos.maciej.codes/extras.html
+fn newline_callback(lex: &mut Lexer<Token>) -> Skip {
+    lex.extras.0 += 1;
+    lex.extras.1 = lex.span().end;
+    Skip
+}
+
+
+/// calcula a posição de linha e coluna e retorna a palavra para um token encontrado
+// baseado no exemplo fornecido em: https://logos.maciej.codes/extras.html
+fn token_callback(lex: &mut Lexer<Token>) -> (usize, usize, String) {
+    let line = lex.extras.0;
+    let column = lex.span().start - lex.extras.1;
+    let word = lex.slice();
+    (line, column, word.to_string())
+}
+
+
+/// deriva 3 atributos para o lexer, efetivamente tornando o enum em um lexer Logos
 #[derive(Logos, Debug, PartialEq)]
 
-// macro para especificar que o analisador deve pular comentários e espaços em branco
-#[logos(skip r"#.*|[ \r\t\n\f]+")]
+/// atributo para especificar que o analisador deve pular comentários e espaços em branco
+#[logos(skip r"#.*|[ \r\t\f]+")]
 
+/// atributo para definir um estado interno aos tokens, esse contém a linha e a coluna correspondente ao token
+#[logos(extras = (usize, usize, String))]
+
+/// enum que define o lexer Logos, em que cada item define um tipo de token reconhecível pelo lexer Logos
 pub enum Token {
-    #[regex(r"_[A-Z][a-zA-Z]{2,}")]
-    FunctionId,
+    #[regex(r"\n", newline_callback, priority = 7)]
+    NewLine,
 
-    #[regex(r"[A-Z][a-zA-Z]{2,}")]
-    Identifier,
+    #[regex(r"_[A-Z][a-zA-Z]{2,}", token_callback, priority = 6)]
+    FunctionId((usize, usize, String)),
 
-    #[regex(r"[1-9][0-9]*")]
-    Integer,
+    #[regex(r"[A-Z][a-zA-Z]{2,}", token_callback, priority = 5)]
+    Identifier((usize, usize, String)),
 
-    #[regex(r"elgio|inteiro|zero|comp|enquanto|se|entao|senao|inicio|fim|maior|menor|igual|diferente")]
-    Reserved,
+    #[regex(r"[1-9][0-9]*", token_callback, priority = 4)]
+    Integer((usize, usize, String)),
 
-    #[regex(r"[\.()]")]
-    Punctuation,
+    #[regex(r"elgio|inteiro|zero|comp|enquanto|se|entao|senao|inicio|fim|maior|menor|igual|diferente", token_callback, priority = 3)]
+    Reserved((usize, usize, String)),
 
-    #[regex(r"[\+\-x=\/]")]
-    Operator,
+    #[regex(r"[\.(),]", token_callback, priority = 2)]
+    Punctuation((usize, usize, String)),
+
+    #[regex(r"[\+\-x=\/]", token_callback, priority = 1)]
+    Operator((usize, usize, String)),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-// importa para o escopo da main o código do módulo lexer (arquivo lexer.rs)
+// importa o código do modulo lexer (arquivo lexer.rs) para o escopo da main
 pub mod lexer;
 
+// importa a struct Logos da crate logos no escopo da main
 use logos::Logos;
+
+// usa as implementações da crate std (standard) do Rust, que é inclusa naturalmente na linguagem
 use std::io::stdin;
 use std::io::Read;
 use std::fs::File;
@@ -9,30 +12,46 @@ use std::fs::File;
 fn main() {
     println!("Analisador Léxico Sintático\n");
 
-    // permite ao usuário selecionar o nome do arquivo com o código fonte Elgol :P
+    // lê a entrada do usuário contendo o caminho do código fonte Elgol, associa essa entrada à variável mutável definida abaixo e trata possíveis erros
     println!("Informe o nome do arquivo com código Elgol:");
     let mut file_name = String::new();
-
-    // lê a entrada do usuário, a associa à variável mutável definida acima e trata possíveis erros
     stdin()
         .read_line(&mut file_name)
         .expect("\nNão foi possível ler a entrada");
     file_name = file_name.trim().to_string();
 
+    // abre o arquivo com o caminho fornecido pelo usuário, associa o arquivo à variável mutável definida abaixo e lida com possíveis erros
     let mut haystack = String::new();
     let mut file_result = File::open(file_name).unwrap();
     file_result
         .read_to_string(&mut haystack)
         .unwrap();
 
+    // teste para visualização do arquivo com código-fonte Elgol
     // println!("\n{:?}", haystack);
 
-    let lexer = lexer::Token::lexer(&haystack);
+    // instancia uma struct de analisador léxico do Logos passando o texto que deve ser analisado
+    let mut lexer = lexer::Token::lexer(&haystack);
 
-    for result in lexer {
-        match result {
-            Ok(token) => println!("{:#?}", token),
-            Err(e) => continue,
+    // exibe cada token gerado pelo analisador léxico, lidando com possíveis erros ao exibir os tokens
+    while let Some(token) = lexer.next() {
+        if let Ok(lexer::Token::FunctionId((line, column, ref word))) = token {
+            println!("<FunctionId: {}, {}:{}>", word, line, column);
+        }
+        if let Ok(lexer::Token::Identifier((line, column, ref word))) = token {
+            println!("<Identifier: {}, {}:{}>", word, line, column);
+        }
+        if let Ok(lexer::Token::Integer((line, column, ref word))) = token {
+            println!("<Integer: {}, {}:{}>", word, line, column);
+        }
+        if let Ok(lexer::Token::Reserved((line, column, ref word))) = token {
+            println!("<Reserved: {}, {}:{}>", word, line, column);
+        }
+        if let Ok(lexer::Token::Punctuation((line, column, ref word))) = token {
+            println!("<Punctuation: {}, {}:{}>", word, line, column);
+        }
+        if let Ok(lexer::Token::Operator((line, column, ref word))) = token {
+            println!("<Operator: {}, {}:{}>", word, line, column);
         }
     }
 }
